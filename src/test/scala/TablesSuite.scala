bde036d1ef18fa669ac44b23b9ce8bf37104d28f
ariel <alvarez.ariel.work@gmail.com>
2014-08-25 17:39:52 -0300

    First Commit - Agregado test básico con conector a H2 (base de datos relacional en memoria)

---
+++ src/test/scala/TablesSuite.scala
@@ -0,0 +1,38 @@
+import java.sql.{ResultSet, Statement, DriverManager, Connection}
+import org.h2.tools.DeleteDbFiles
+import org.scalatest._
+import scala.slick.driver.H2Driver.simple._
+import scala.slick.jdbc.meta._
+import scala.slick.jdbc.StaticQuery.interpolation
+
+
+class TablesSuite extends FunSuite with BeforeAndAfter {
+
+  implicit var conn: Connection = _
+
+  before {
+    DeleteDbFiles.execute("~", "test", true)
+    Class.forName("org.h2.Driver")
+    conn = DriverManager.getConnection("jdbc:h2:~/test")
+  }
+  
+  test("Creating the Schema works") {
+
+    val stat:Statement = conn.createStatement()
+
+    stat.execute("create table test(id int primary key, name varchar(255))")
+    stat.execute("insert into test values(1, 'Hello')")
+
+    val rs:ResultSet = stat.executeQuery("select * from test")
+    rs.next()
+    assert( rs.getString("name") === "Hello")
+
+    stat.close()
+  }
+
+  
+  after {
+    conn.close()
+  }
+
+}
