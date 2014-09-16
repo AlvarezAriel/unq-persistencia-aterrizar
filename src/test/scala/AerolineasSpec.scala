da2083b7872824f6c95c17f10627d888355b71c8
nahuelbe <beniteznahueloscar@gmail.com>
2014-09-15 21:18:38 -0300

    Agregado home generator generico

--- src/test/scala/AerolineasSpec.scala
+++ src/test/scala/AerolineasSpec.scala
@@ -1,11 +1,15 @@
 import edu.unq.persistencia.cake.component.HomeComponentJPA
 import edu.unq.persistencia.DefaultSessionProviderComponent
-import edu.unq.persistencia.model.Asiento
+import edu.unq.persistencia.model.{Entity, Asiento}
 import org.scalatest.{BeforeAndAfter, Matchers, FlatSpec}
 
 class AerolineasSpec  extends FlatSpec with Matchers with BeforeAndAfter {
   val asientosHome = new HomeComponentJPA[Asiento] with DefaultSessionProviderComponent {override val clazz: Class[Asiento] = classOf[Asiento]}
 
+
+  def homeGenerator[T <: Entity[_]](clase:Class[T]) = {
+    new HomeComponentJPA[T] with DefaultSessionProviderComponent {override val clazz: Class[T] = clase}
+  }
   before {
 //    asientosHome = new HomeComponentJPA[Asiento] with DefaultSessionProviderComponent {override val clazz: Class[Asiento] = classOf[Asiento]}
   }
@@ -20,5 +24,6 @@ class AerolineasSpec  extends FlatSpec with Matchers with BeforeAndAfter {
     asientosHome.updater.save(asiento)
     asientosHome.updater.save(asiento)
     asientosHome.updater.save(asiento)
+    homeGenerator(classOf[Asiento]).updater.save(asiento)
   }
 }
