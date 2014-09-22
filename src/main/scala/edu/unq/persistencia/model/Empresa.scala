fdf0e38a2345fa1121a08564b9d4848f9cfc3951
ariel <alvarez.ariel.work@gmail.com>
2014-09-22 20:58:57 -0300

    Reemplazadas las colecciones inmutables por mutables, y agregado el import del implicit para conversiones

--- src/main/scala/edu/unq/persistencia/model/Empresa.scala
+++ src/main/scala/edu/unq/persistencia/model/Empresa.scala
@@ -4,10 +4,11 @@ import edu.unq.persistencia.model.login.UsuarioEntity
 import scala.beans.BeanProperty
 import scala.collection._
 import org.joda.time.DateTime
+import scala.collection.JavaConversions._
 
 trait Identificable {@BeanProperty var id:Long = 0  }
 
-class Empresa(@BeanProperty var aerolineas:mutable.Seq[Aerolinea]) extends Identificable
+class Empresa(@BeanProperty var aerolineas:java.util.Set[Aerolinea]) extends Identificable
 
 trait Categoria extends Identificable with Serializable { val factor:BigDecimal}
 object Business extends Categoria {val factor:BigDecimal = 0.3}
@@ -15,22 +16,22 @@ object Primera extends Categoria  {val factor:BigDecimal = 0.5}
 object Turista extends Categoria  {val factor:BigDecimal = 0.2}
 
 class Aerolinea extends Identificable {
-  @BeanProperty var vuelos:mutable.Seq[Vuelo] = _
+  @BeanProperty var vuelos:java.util.Set[Vuelo] = _
   //  def getVuelos = vuelos
   //  def setVuelos(vuelos:Seq[Vuelo]) = this.vuelos = vuelos
 }
 
 class Asiento extends Entity[Asiento] {
    @BeanProperty var numero:   Int = _
-   @BeanProperty var categoria:Categoria = _
+   @BeanProperty var categoria:Categoria = Turista
    @BeanProperty var pasajero: UsuarioEntity = _
    @BeanProperty var tramo:    Tramo = _
 
 }
 
 class Vuelo extends Identificable {
-  @BeanProperty var tramos:mutable.Seq[Tramo] = mutable.Seq.empty[Tramo]
-  def esDirecto: Boolean = tramos.length == 1
+  @BeanProperty var tramos:java.util.Set[Tramo] = Set.empty[Tramo]
+  def esDirecto: Boolean = tramos.size() == 1
 }
 
 class Tramo extends Entity[Tramo] {
@@ -39,7 +40,7 @@ class Tramo extends Entity[Tramo] {
   @BeanProperty var salida     :DateTime = DateTime.now()
   @BeanProperty var llegada    :DateTime = DateTime.now()
   @BeanProperty var precioBase :java.math.BigDecimal = _
-  @BeanProperty var asientos   :mutable.Seq[Asiento] = mutable.Seq.empty[Asiento]
+  @BeanProperty var asientos   :java.util.Set[Asiento] = Set.empty[Asiento]
 
 }
 
@@ -48,7 +49,7 @@ class Locacion extends Identificable {@BeanProperty var nombre:String = ""}
 /***************************************************************************************/
 /******************************* COMPANION OBJECTS *************************************/
 
-object Vuelo { def apply(tramos:mutable.Seq[Tramo]) = {val esto = new Vuelo();esto.tramos=tramos;esto}}
+object Vuelo { def apply(tramos:java.util.Set[Tramo]) = {val esto = new Vuelo();esto.tramos=tramos;esto}}
 
 
 object Tramo {
